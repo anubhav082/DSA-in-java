007136888ada4487efb10b8b330cd4b72c03054d
Anubhav Shukla <91797411+anubhav082@users.noreply.github.com>
2021-10-03 13:56:38 +0530

    Update BitWise.java

--- src/BitWise.java
+++ src/BitWise.java
@@ -1,15 +1,15 @@
 public class BitWise {
     public int divideBy2(int x) {
-        return x >> 1;
+        return x >> 1; // returning the value
 
     }
 
     public int multiplyBy2(int x) {
-        return x << 1;
+        return x << 1; 
     }
 
     public int log2(int x) {
-        int res = 0;
+        int res = 0; // res is container
         while ((x >>= 1) != 0)
             res++;
 
